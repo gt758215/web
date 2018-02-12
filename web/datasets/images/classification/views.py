1164ff9cf9b25e1d8aeea3497100926f7c56450c
Simon Chuang <simon.s.chuang@gmail.com>
2018-02-13 00:22:10 +0800

    add layout.html

--- web/datasets/images/classification/views.py
+++ web/datasets/images/classification/views.py
@@ -3,10 +3,28 @@ import shutil
 import flask
 from flask import Blueprint, render_template
 from .forms import ImageClassificationDatasetForm
+from web.datasets import DatasetJob
 from web import utils
 from web.datasets.images.forms import ImageDatasetForm
 from web.datasets.images.job import ImageDatasetJob
 blueprint = Blueprint(__name__, __name__)
+
+
+@blueprint.route('/', methods=['GET'])
+def summary():
+    try:
+        running_datasets = get_job_list(DatasetJob, True)
+        return render_template('datasets.html', running_datasets=running_datasets)
+    except TemplateNotFound:
+        abort(404)
+
+
+def get_job_list(cls, running):
+    return sorted(
+        [j for j in scheduler.jobs.values() if isinstance(j, cls) and j.status.is_running() == running],
+        key=lambda j: j.status_history[0][1],
+        reverse=True,
+    )
 
 
 @blueprint.route('/new', methods=['GET'])
