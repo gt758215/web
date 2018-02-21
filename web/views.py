aa2f09462c7389c7092efe0d4e64a93bee7c0164
SimonChuang <simon.s.chuang@gmail.com>
2018-02-21 17:53:11 +0800

    add path_autocomplete()

--- web/views.py
+++ web/views.py
@@ -1,5 +1,10 @@
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
+import flask
+import os
+import platform
+import json
+import glob
 
 blueprint = Blueprint(__name__, __name__)
 
@@ -9,3 +14,31 @@ def home(tab=2):
         return render_template('home.html')
     except TemplateNotFound:
         abort(404)
+
+
+
+# Path Completion
+
+
+@blueprint.route('/autocomplete/path', methods=['GET'])
+def path_autocomplete():
+    """
+    Return a list of paths matching the specified preamble
+
+    """
+    path = flask.request.args.get('query', '')
+
+    if not os.path.isabs(path):
+        # Only allow absolute paths by prepending forward slash
+        path = os.path.sep + path
+
+    suggestions = [os.path.abspath(p) for p in glob.glob(path + "*")]
+    if platform.system() == 'Windows':
+        # on windows, convert backslashes with forward slashes
+        suggestions = [p.replace('\\', '/') for p in suggestions]
+
+    result = {
+        "suggestions": sorted(suggestions)
+    }
+
+    return json.dumps(result)
