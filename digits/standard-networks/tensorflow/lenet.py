513999b397ef469f832e9527d1a5170d00f07420
Simon Chuang <simon.s.chuang@gmail.com>
2018-03-28 14:23:09 +0800

    Add support for tensorpack

--- digits/standard-networks/tensorflow/lenet.py
+++ digits/standard-networks/tensorflow/lenet.py
@@ -1,34 +1,74 @@
-from model import Tower
-from utils import model_property
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File: mnist-tfslim.py
+
+"""
+MNIST ConvNet example using TensorFlow-slim.
+Mostly the same as 'mnist-convnet.py',
+the only differences are:
+    1. use slim.layers, slim.arg_scope, etc
+    2. use slim names to summarize weights
+"""
+
+
+from tensorpack import *
+from tensorpack.dataflow import dataset
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
-import utils as digits
+
+IMAGE_SIZE = 28
 
 
-class UserModel(Tower):
+class Model(ModelDesc):
+    def _get_inputs(self):
+        return [InputDesc(tf.float32, (None, IMAGE_SIZE, IMAGE_SIZE), 'input'),
+                InputDesc(tf.int32, (None,), 'label')]
 
-    @model_property
-    def inference(self):
-        x = tf.reshape(self.x, shape=[-1, self.input_shape[0], self.input_shape[1], self.input_shape[2]])
-        # scale (divide by MNIST std)
-        x = x * 0.0125
-        with slim.arg_scope([slim.conv2d, slim.fully_connected],
-                            weights_initializer=tf.contrib.layers.xavier_initializer(),
-                            weights_regularizer=slim.l2_regularizer(0.0005)):
-            model = slim.conv2d(x, 20, [5, 5], padding='VALID', scope='conv1')
-            model = slim.max_pool2d(model, [2, 2], padding='VALID', scope='pool1')
-            model = slim.conv2d(model, 50, [5, 5], padding='VALID', scope='conv2')
-            model = slim.max_pool2d(model, [2, 2], padding='VALID', scope='pool2')
-            model = slim.flatten(model)
-            model = slim.fully_connected(model, 500, scope='fc1')
-            model = slim.dropout(model, 0.5, is_training=self.is_training, scope='do1')
-            model = slim.fully_connected(model, self.nclasses, activation_fn=None, scope='fc2')
-            return model
+    def _build_graph(self, inputs):
+        image, label = inputs
+        image = tf.expand_dims(image, 3)
 
-    @model_property
-    def loss(self):
-        model = self.inference
-        loss = digits.classification_loss(model, self.y)
-        accuracy = digits.classification_accuracy(model, self.y)
-        self.summaries.append(tf.summary.scalar(accuracy.op.name, accuracy))
-        return loss
+        image = image * 2 - 1
+
+        is_training = get_current_tower_context().is_training
+        with slim.arg_scope([slim.layers.fully_connected],
+                            weights_regularizer=slim.l2_regularizer(1e-5)):
+            l = slim.layers.conv2d(image, 32, [3, 3], scope='conv0')
+            l = slim.layers.max_pool2d(l, [2, 2], scope='pool0')
+            l = slim.layers.conv2d(l, 32, [3, 3], padding='SAME', scope='conv1')
+            l = slim.layers.conv2d(l, 32, [3, 3], scope='conv2')
+            l = slim.layers.max_pool2d(l, [2, 2], scope='pool1')
+            l = slim.layers.conv2d(l, 32, [3, 3], scope='conv3')
+            l = slim.layers.flatten(l, scope='flatten')
+            l = slim.layers.fully_connected(l, 512, scope='fc0')
+            l = slim.layers.dropout(l, is_training=is_training)
+            logits = slim.layers.fully_connected(l, 10, activation_fn=None, scope='fc1')
+
+        tf.nn.softmax(logits, name='prob')
+
+        cost = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=label)
+        cost = tf.reduce_mean(cost, name='cross_entropy_loss')
+
+        acc = tf.to_float(tf.nn.in_top_k(logits, label, 1))
+
+        acc = tf.reduce_mean(acc, name='accuracy')
+        summary.add_moving_summary(acc)
+
+        self.cost = cost
+        summary.add_moving_summary(cost)
+        summary.add_param_summary(('.*/weights', ['histogram', 'rms']))  # slim uses different variable names
+
+    def _get_optimizer(self):
+        lr = tf.train.exponential_decay(
+            learning_rate=1e-3,
+            global_step=get_global_step_var(),
+            decay_steps=468 * 10,
+            decay_rate=0.3, staircase=True, name='learning_rate')
+        tf.summary.scalar('lr', lr)
+        return tf.train.AdamOptimizer(lr)
+
+
+def get_data():
+    train = BatchData(dataset.Mnist('train'), 128)
+    test = BatchData(dataset.Mnist('test'), 256, remainder=True)
+    return train, test
