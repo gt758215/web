95b7a4e5e71501e197fb20965a6d72c171eeaa1b
Simon Chuang <simon.s.chuang@gmail.com>
2018-06-07 10:35:35 +0800

    rename tools/tensorflow to tool/tf to avoid python import conflict. fix tensorboard can't display issue.

--- digits/model/tasks/tensorflow_train.py
+++ digits/model/tasks/tensorflow_train.py
@@ -175,7 +175,7 @@ class TensorflowTrainTask(TrainTask):
     def task_arguments(self, resources, env):
 
         args = [sys.executable,
-                os.path.join(os.path.dirname(os.path.abspath(digits.__file__)), 'tools', 'tensorflow', 'main.py'),
+                os.path.join(os.path.dirname(os.path.abspath(digits.__file__)), 'tools', 'tf', 'main.py'),
                 '--network=%s' % self.model_file,
                 '--epoch=%d' % int(self.train_epochs),
                 '--networkDirectory=%s' % self.job_dir,
@@ -563,7 +563,7 @@ class TensorflowTrainTask(TrainTask):
         file_to_load = self.get_snapshot(snapshot_epoch)
 
         args = [sys.executable,
-                os.path.join(os.path.dirname(os.path.abspath(digits.__file__)), 'tools', 'tensorflow', 'main.py'),
+                os.path.join(os.path.dirname(os.path.abspath(digits.__file__)), 'tools', 'tf', 'main.py'),
                 '--inference_db=%s' % temp_image_path,
                 '--network=%s' % self.model_file,
                 '--networkDirectory=%s' % self.job_dir,
@@ -861,7 +861,7 @@ class TensorflowTrainTask(TrainTask):
             file_to_load = self.get_snapshot(snapshot_epoch)
 
             args = [sys.executable,
-                    os.path.join(os.path.dirname(os.path.abspath(digits.__file__)), 'tools', 'tensorflow', 'main.py'),
+                    os.path.join(os.path.dirname(os.path.abspath(digits.__file__)), 'tools', 'tf', 'main.py'),
                     '--testMany=1',
                     '--allPredictions=1',  # all predictions are grabbed and formatted as required by DIGITS
                     '--inference_db=%s' % str(temp_dir_path),
